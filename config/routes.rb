71211dde8b1f61f83038c892d3f1b25ccd191121
Masaki_Nishihara <n.m.s.s.11301130@icloud.com>
2021-06-10 11:50:13 +0900

    rootの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+
+  root  'users#index' 
+
+
+
+
   resources :feeds
   # get 'sessions/new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+
+
+
 
 
 end
